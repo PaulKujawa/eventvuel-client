404195dbf0869f0ef8dd2a5a80fbb7fd2fa21b05
Paul Kujawa <p.kujawa@gmx.net>
2018-09-18 09:44:35 +0200

    vue setup with router plus TS and babel (#1)

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    
+    <Greeting
+      name="Paul">     
+    </Greeting>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+import Greeting from '@/components/Greeting.vue';
+
+@Component({
+  components: {
+    Greeting,
+  },
+})
+export default class Home extends Vue {
+}
+</script>
