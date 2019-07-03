06a06ca7a493632706f832bf04a93eb82244e4ae
Paul Kujawa <p.kujawa@gmx.net>
2019-07-03 09:13:13 +0200

    add ScreenSizeMixin for responsive img height

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Vue, Vuetify & SASS, Apollo (GraphQl), TS, Webpack, Prettier & TSLint, Netlify
 ## Browser support
 
 - Chrome 71 - market share
-- Edge 17 - market share
+- Edge 16 - passive event listeners
 - Firefox 65 - market share
 - Safari 12.1 - IntersectionObserver
 - iOS Safari 12.2 - IntersectionObserver
--- src/components/CategoryCard.vue
+++ src/components/CategoryCard.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card color="accent" hover :to="{ name: category.routeName }">
-    <v-img :src="category.imgPath"></v-img>
+    <v-img :height="imageHeight" :src="category.imgPath"></v-img>
 
     <v-card-title class="justify-center">
       <h3 class="headline">{{ category.title }}</h3>
@@ -9,11 +9,19 @@
 </template>
 
 <script lang="ts">
+import ScreenSizeMixin from "@/mixins/screen-size";
 import { Category } from "@/models/category";
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { mixins } from "vue-class-component";
+import { Component, Prop, Watch } from "vue-property-decorator";
 
 @Component
-export default class CategoryCard extends Vue {
+export default class CategoryCard extends mixins(ScreenSizeMixin) {
   @Prop() public readonly category!: Category;
+  public imageHeight = 0;
+
+  @Watch("windowWidth", { immediate: true })
+  public onResize(width: number): void {
+    this.imageHeight = width > 960 ? 550 : 400;
+  }
 }
 </script>
